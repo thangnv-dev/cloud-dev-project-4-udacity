cb6afe75120335cd71b09119e8a3d6448d54314e
thangnv-dev <nvthangf0909@gmail.com>
2024-08-26 23:30:32 +0700

    Update todosAccess

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,11 +1,12 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'arn9s7gm2j'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+export const apiEndpoint =
+  process.env.REACT_APP_API_ENDPOINT ||
+  `https://arn9s7gm2j.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: 'dev-qa5fgz4jnp365mzs.us.auth0.com', // Auth0 domain
-  clientId: 'YsXWhHkVmjyXepGdGeXE4Fsac0ZTKl1E', // Auth0 client id
+  domain: process.env.REACT_APP_AUTH0_DOMAIN, // Auth0 domain
+  clientId: process.env.REACT_APP_AUTH0_CLIENT_ID, // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
